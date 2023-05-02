90cda3745a534febcadea9e33e9da67ee6ab45f6
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@test.com>
2023-05-02 11:01:18 -0300

    Programacao form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent {
+  cadastroForm: FormGroup;
 
+  constructor(private builder: FormBuilder){
+    this.cadastroForm = builder.group({
+      nome: [''],
+      usuario: [''],
+      email: [''],
+      senha: [''],
+      confirmaSenha: [''],
+      aceite: [false]
+    })
+  }
+
+  cadastrar() {
+    console.log(this.cadastroForm.value);
+  }
 }
