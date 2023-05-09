cdba86414824e52bddbd92b58a0258169843421e
VitorHenrique-ViniciusFaoro <VitorHenrique-ViniciusFaoro@test.com>
2023-05-09 11:05:42 -0300

    09/05

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,12 +12,18 @@ export class CadastroComponent {
   constructor(private builder: FormBuilder){
     this.cadastroForm = builder.group({
       nome: [''],
-      usuario: [''],
-      email: [''],
-      senha: [''],
-      confirmaSenha: [''],
-      aceite: [false]
-    })
+      usuario: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(15)] ],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      confirmaSenha: ['', Validators.required],
+      aceite: [false, Validators.requiredTrue]
+    }, {validators: this.checkPasswords})
+  }
+
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => {
+    let pass = group.get('senha')?.value;
+    let confirmPass = group.get('confirmaSenha')?.value
+    return pass === confirmPass ? null : { notSame: true }
   }
 
   cadastrar() {
